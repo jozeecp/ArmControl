a8a9d8ba4a107bd95b39aa84667812a60a440aff
Jose <jfcatarino1@gmail.com>
2017-04-29 16:55:23 -0600

    v1.0.1
    Modified filter function as int function. Untested.

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -4,9 +4,11 @@
 
 Previous Version
   v0.0                                 // imported from months ago
+  v1.0                                 // created filter function
+
 
 Current Version
-  v1.0                                 // created filter function
+  v1.0.1                               // 4/20/17, modified filter fn as int fn
 */
 
 #include <Servo.h>
@@ -16,6 +18,7 @@ Servo myservo;  // create servo object to control a servo
 int sensorPin = 0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
 
+// values for the average function
 int g1 = 0;
 int g2 = 0;
 int g3 = 0;
@@ -29,24 +32,23 @@ void setup() {
 }
 
 void loop() {
-  val = analogRead(sensorPin);            // reads the value of the sensor (value between 60 and 200)
+  val = analogRead(sensorPin);         // reads the value of the sensor (value between 60 and 200)
   val = map(val, 60, 200, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
 
-  avgFilter();                         //filter function
-
-  myservo.write(g);                    // sets the servo position according to the scaled value
-  delay(50);                           // waits for the servo to get there
+  myservo.write(avgFilter(val));             // sets the servo position according to the scaled value
 
   Serial.print(val);
   Serial.print("   ");
   Serial.println(g);
 }
 
-void avgFilter () {
+// avgFilter(variable to average)
+int avgFilter (int a) {
   g5 = g4;
   g4 = g3;
   g3 = g2;
   g2 = g1;
-  g1 = val;
+  g1 = a;
   g = (g1 + g2 + g3 + g4 + g5) / 5;
+  return g;
 }
